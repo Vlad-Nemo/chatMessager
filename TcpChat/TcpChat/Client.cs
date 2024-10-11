b0d53dc03c3333dd46c9ed80c902365e54bab200
Vlad-Nemo <mironovvladik1307@gmail.com>
2024-10-11 20:32:38 +0700

    Create TcpChat

---
+++ TcpChat/TcpChat/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+
+namespace TcpChat
+{
+    public class Client
+    {
+        private TcpClient client;
+        private Thread receiveThread;
+        public event Action<string> MessageReceived;
+        public void Start(string ipAddress, int port)
+        {
+            client = new TcpClient();
+            client.Connect(ipAddress, port);
+            receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+            receiveThread.Start();
+        }
+        private void ReceiveMessages()
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            string serverMessage;
+            while ((serverMessage = reader.ReadLine()) != null)
+            {
+                MessageReceived?.Invoke(serverMessage);
+            }
+            client.Close();
+        }
+        public void SendMessage(string message)
+        {
+            StreamWriter writer = new StreamWriter(client.GetStream())
+            {
+                AutoFlush = true
+            };
+            writer.WriteLine(message);
+        }
+    }
+}
